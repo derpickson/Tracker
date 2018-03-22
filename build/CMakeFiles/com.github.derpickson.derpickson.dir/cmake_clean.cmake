d248570604287512f8cdfe08172966e1ef02e2e0
derpickson <derpickson@protonmail.com>
2018-03-22 16:50:31 -0700

    Added Build Files

---
+++ build/CMakeFiles/com.github.derpickson.derpickson.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/gtk-hello.c"
+  "com.github.derpickson.derpickson_valac.stamp"
+  "CMakeFiles/com.github.derpickson.derpickson.dir/src/gtk-hello.c.o"
+  "com.github.derpickson.derpickson.pdb"
+  "com.github.derpickson.derpickson"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.derpickson.derpickson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
