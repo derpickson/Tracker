6365233eb2fd48c7374d7cd51fe520be5ca54bfb
derpickson <derpickson@protonmail.com>
2018-03-22 22:34:21 -0700

    Added translation files

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -72,6 +72,12 @@ if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspeci
 file(INSTALL DESTINATION "/usr/share/metainfo" TYPE FILE FILES "/home/nick/Projects/derpickson/data/com.github.derpickson.derpickson.appdata.xml")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/nick/Projects/derpickson/build/po/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
